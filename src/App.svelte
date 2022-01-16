417b9aef67734d0177d83e793452d2727f3e6b9c
jtkaufman737 <jtkaufman737@gmail.com>
2022-01-16 14:32:49 -0500

    Pare down file contents for boilerplate

--- src/App.svelte
+++ src/App.svelte
@@ -1,77 +1,73 @@
-<script>
-	import { onMount } from "svelte";
+<script></script>
 
-	let pokemon = [];
-	const db = $$props.db; 
-	const q = $$props.q;
-
-	onMount(async () => {
-		const response = await db.query(
-			q.Map(
-				q.Paginate(q.Documents(q.Collection("pokemon"))),
-				q.Lambda(item => q.Get(item))
-			)
-		) 
-
-		pokemon = response.data.map(item => {
-			return item.data
-		})
-	})
-</script>
-
-<main id="app">
-	<div class="home">
-		<h3>Pokemon</h3>
-		<section class="grid">
-		   {#each pokemon as p}
-			<div>
-				<h6>{p.name}</h6>
-				<img src={p.imageUrl} alt={p.name} class="pokemon"/>
-			</div>
-		   {/each}
-		</section>
-	</div>
-</main>
+<main id="app"></main>
 
 <style>
-#app {
-  font-family: 'Inter', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h3,h6 {
-  font-family: acumin-pro,sans-serif;
-  font-weight: 500;
-  font-style: normal;
-}
-
-h3 {
-  font-size:2em;
-}
-
-h6 {
-  font-size:1em;
-}
-
-div.home {
-  width:60%;
-  margin:auto;
-}
-
-section.grid {
-  display:grid;
-  grid-template-columns:33.33% 33.33% 33.33%;
-  grid-template-rows:auto;
-  margin:auto;
-  padding:5% 20% 5% 20%;
-}
-
-img.pokemon {
-  height:100px;
-  width:100px;
-}
+	#app {
+	  font-family: 'Inter', sans-serif;
+	  -webkit-font-smoothing: antialiased;
+	  -moz-osx-font-smoothing: grayscale;
+	  text-align: center;
+	  color: #2c3e50;
+	  margin-top: 60px;
+	}
+	
+	h1,h2,h3,h4,h5,h6 {
+	  font-family: acumin-pro,sans-serif;
+	  font-weight: 500;
+	  font-style: normal;
+	}
+	
+	h3 {
+	  font-size:2em;
+	}
+	
+	h6 {
+	  font-size:1.25em;
+	}
+	
+	div.card {
+	  border-radius:30px;
+	  border:.25px lightgray solid;
+	  margin:5px;
+	}
+	
+	div.header {
+	  width:100%;
+	  color:white;
+	  background: linear-gradient(90deg,#3F00A5 0%,#813EEF 100%);
+	  display:flex;
+	  justify-content:space-around;
+	  align-items:center;
+	  border-top-left-radius:30px;
+	  border-top-right-radius:30px;
+	  height:60px;
+	}
+	
+	a {
+	  color:#3F00A5;
+	}
+	
+	a:hover {
+	  color:#813EEF;
+	}
+	
+	div.home {
+	  width:60%;
+	  margin:auto;
+	}
+	
+	section.grid {
+	  display:grid;
+	  grid-template-columns:33.33% 33.33% 33.33%;
+	  grid-template-rows:auto;
+	  margin:auto;
+	  padding:5% 20% 5% 20%;
+	}
+	
+	img.pokemon {
+	  height:100px;
+	  width:100px;
+	  padding:25px 10px;
+	}
 </style>
